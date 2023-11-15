e968b5af20efad2a681cf88de7e50a2f33bc8c9f
Soohyun Lim <d2axasap@gmail.com>
2023-11-14 23:31:58 -0500

    Embed a map for plane tour routes.

--- src/components/ManhattanPlaneTour.jsx
+++ src/components/ManhattanPlaneTour.jsx
@@ -1,6 +1,9 @@
 const ManhattanPlaneTour = () => {
   return (
-    <h2>Manhattan Plane Tour page content</h2>
+    <>
+      <h2>Manhattan Plane Tour page content</h2>
+      {/* <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1U0G9YNmFHTlpQDVgtA876USNWQ94De0&ehbc=2E312F&noprof=1" width="640" height="480"></iframe> */}
+    </>
   );
 };
 
