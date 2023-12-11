58a568b3aacda7210249e36107d6c6663669812c
Soohyun Lim <d2axasap@gmail.com>
2023-12-11 17:07:29 -0500

    Add carousels to home page. Continue working on home page layout.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,37 @@
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 const Home = () => {
+
+  const imagesCarousel = () => (
+    <Carousel autoPlay interval="10000" infiniteLoop showThumbs={false}>
+      <div>
+        <img src="https://placehold.co/100x100" />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img src="https://placehold.co/200x200" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img src="https://placehold.co/300x300" />
+        <p className="legend">Legend 3</p>
+      </div>
+    </Carousel>
+  );
+
   return (
-    <div className='page-wrapper'>
-      <h2>Home page content</h2>
-    </div>
+    <>
+      <Carousel showThumbs={false}>
+        <div>
+          <img src="https://placehold.co/100x100" />
+        </div>
+      </Carousel>
+      {imagesCarousel()}
+      <div className='page-wrapper'></div>
+
+    </>
+
   );
 };
 
