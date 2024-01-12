414176be09994dcb49034c84219c2f764cade38f
Soohyun Lim <d2axasap@gmail.com>
2024-01-12 13:18:15 -0500

    Add grid gallary and ligth box to gallery page. Include images for gallery page as well.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,52 @@
+import TopImageSection from './TopImageSection';
+import { useState } from 'react';
+import { Gallery as GridGallery } from 'react-grid-gallery';
+import Lightbox from 'react-18-image-lightbox';
+import "react-18-image-lightbox/style.css";
+import { IMAGES } from './Images';
+
+
 const Gallery = () => {
+  const [index, setIndex] = useState(-1);
+
+  const currentImage = IMAGES[index];
+  const nextIndex = (index + 1) % IMAGES.length;
+  const nextImage = IMAGES[nextIndex] || currentImage;
+  const prevIndex = (index + IMAGES.length - 1) % IMAGES.length;
+  const prevImage = IMAGES[prevIndex] || currentImage;
+
+  const handleClick = (index, item) => setIndex(index);
+  const handleClose = () => setIndex(-1);
+  const handleMovePrev = () => setIndex(prevIndex);
+  const handleMoveNext = () => setIndex(nextIndex);
+
   return (
-    <div className='page-wrapper'>
-      <h2>Gallery page content</h2>
-    </div>
+    <main className='page-container'>
+      <TopImageSection title='Gallery' />
+      <div className='gallery-container'>
+        <GridGallery
+          images={IMAGES}
+          onClick={handleClick}
+          enableImageSelection={false}
+          rowHeight={450}
+          tagStyle={{ color: 'white', fontSize: '12px', padding: '2.4px 7.2px 3.6px', backgroundColor: "rgba(0, 0, 0, 0.8)" }}
+        />
+        {!!currentImage && (
+          <Lightbox
+            mainSrc={currentImage.original}
+            imageTitle={currentImage.caption}
+            mainSrcThumbnail={currentImage.src}
+            nextSrc={nextImage.original}
+            nextSrcThumbnail={nextImage.src}
+            prevSrc={prevImage.original}
+            prevSrcThumbnail={prevImage.src}
+            onCloseRequest={handleClose}
+            onMovePrevRequest={handleMovePrev}
+            onMoveNextRequest={handleMoveNext}
+          />
+        )}
+      </div>
+    </main>
   );
 };
 
---
+++ src/components/Images.js
@@ -0,0 +1,108 @@
+import gallery1 from "../assets/images/gallery/gallery-1.jpg";
+import gallery2 from "../assets/images/gallery/gallery-2.jpg";
+import gallery3 from "../assets/images/gallery/gallery-3.jpg";
+import gallery4 from "../assets/images/gallery/gallery-4.jpg";
+import gallery5 from "../assets/images/gallery/gallery-5.jpg";
+import gallery6 from "../assets/images/gallery/gallery-6.jpg";
+import gallery7 from "../assets/images/gallery/gallery-7.jpg";
+import gallery8 from "../assets/images/gallery/gallery-8.jpg";
+import gallery9 from "../assets/images/gallery/gallery-9.jpg";
+import gallery10 from "../assets/images/gallery/gallery-10.jpg";
+
+export const IMAGES = [
+  {
+    src: gallery1,
+    original: gallery1,
+    width: 320,
+    height: 174,
+    tags: [
+      { value: "Manhattan", title: "Manhattan" },
+      { value: "Chrysler Building", title: "Chrysler Building" },
+    ]
+  },
+  {
+    src: gallery2,
+    original: gallery2,
+    width: 320,
+    height: 212,
+    tags: [
+      { value: "Robert Moses Beach", title: "Robert Moses Beach" },
+      { value: "Fire Island", title: "Fire Island" }
+    ]
+  },
+  {
+    src: gallery3,
+    original: gallery3,
+    width: 320,
+    height: 212,
+    tags: [
+      { value: "East River", title: "East River" },
+      { value: "Manhattan Bridge", title: "Manhattan Bridge" },
+    ]
+  },
+  {
+    src: gallery4,
+    original: gallery4,
+    width: 320,
+    height: 213,
+    tags: [
+      { value: "Flatiron District", title: "Flatiron District" }
+    ],
+  },
+  {
+    src: gallery5,
+    original: gallery5,
+    width: 320,
+    height: 183,
+    tags: [
+      { value: "Liberty Island", title: "Liberty Island" },
+      { value: "Statue of Liberty", title: "Statue of Liberty" },
+    ]
+  },
+  {
+    src: gallery6,
+    original: gallery6,
+    width: 240,
+    height: 320,
+    tags: [
+      { value: "Empire State Building", title: "Empire State Building" }
+    ],
+  },
+  {
+    src: gallery7,
+    original: gallery7,
+    width: 320,
+    height: 190,
+    tags: [
+      { value: "Hudson River", title: "Hudson River" },
+      { value: "George Washington Bridge", title: "George Washington Bridge" },
+    ]
+  },
+  {
+    src: gallery8,
+    original: gallery8,
+    width: 250,
+    height: 148,
+    tags: [{ value: "Jones Beach", title: "Jones Beach" }],
+  },
+  {
+    src: gallery9,
+    original: gallery9,
+    width: 320,
+    height: 213,
+    tags: [
+      { value: "Montauk", title: "Montauk" },
+      { value: "Lighthouse", title: "Lighthouse" },
+    ]
+  },
+  {
+    src: gallery10,
+    original: gallery10,
+    width: 570,
+    height: 320,
+    tags: [
+      { value: "Little Island", title: "Little Island" },
+      { value: "Hudson River", title: "Hudson River" },
+    ]
+  },
+];
