5bcd1fd8b7d1bbedf06fc909905691fbdaa69249
Soohyun Lim <d2axasap@gmail.com>
2024-01-15 17:04:47 -0500

    Replace value list section to about tour section in home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,27 +37,27 @@ const tours = [
   },
 ];
 
-const values = [
-  {
-    title: 'safety first',
-    description: 'Safety is paramount! We fly Bell407 planes that include the state-of-the-art TCAS system (Traffic Collision Avoidance System) maintained by our in-house team of factory-certified technicians! On site, we ensure you receive a safety briefing and are always escorted to your aircraft by our trained ground crew.',
-  },
-  {
-    title: 'experienced pilots',
-    description: 'Jet Air Blue conducts regular recurring training to ensure our pilots are ready to take you on a journey you will never forget. Our pilots come to Jet Air Blue with at least 1,000 flight hours, and their training ensures they know the Bell407 aircraft inside and out, not to mention some facts about NYC they can share with you on your tour.',
-  },
-  {
-    title: 'quality aircraft',
-    description: 'Jet Air Blue flies the Bell407, a 6-seat aircraft that is renowned for its safety and comfort. The Bell407 is the most popular helicopter in the world, with over 1,600 flying worldwide. The Bell407 is equipped with a Rolls Royce engine, and is known for its reliability and smooth ride.',
-  },
-  {
-    title: '5 star service',
-    description: 'Our service is how we stand out! We understand that your Jet Air Blue experience begins the first time you contact us. From our reservations, ground crew and pilots, we strive to ensure you have a stress-free and exciting experience. Our attention to customer satisfaction is why we’re rated 5 stars on TripAdvisor!',
-  },
-];
+// const values = [
+//   {
+//     title: 'safety first',
+//     description: 'Safety is paramount! We fly Bell407 planes that include the state-of-the-art TCAS system (Traffic Collision Avoidance System) maintained by our in-house team of factory-certified technicians! On site, we ensure you receive a safety briefing and are always escorted to your aircraft by our trained ground crew.',
+//   },
+//   {
+//     title: 'experienced pilots',
+//     description: 'Jet Air Blue conducts regular recurring training to ensure our pilots are ready to take you on a journey you will never forget. Our pilots come to Jet Air Blue with at least 1,000 flight hours, and their training ensures they know the Bell407 aircraft inside and out, not to mention some facts about NYC they can share with you on your tour.',
+//   },
+//   {
+//     title: 'quality aircraft',
+//     description: 'Jet Air Blue flies the Bell407, a 6-seat aircraft that is renowned for its safety and comfort. The Bell407 is the most popular helicopter in the world, with over 1,600 flying worldwide. The Bell407 is equipped with a Rolls Royce engine, and is known for its reliability and smooth ride.',
+//   },
+//   {
+//     title: '5 star service',
+//     description: 'Our service is how we stand out! We understand that your Jet Air Blue experience begins the first time you contact us. From our reservations, ground crew and pilots, we strive to ensure you have a stress-free and exciting experience. Our attention to customer satisfaction is why we’re rated 5 stars on TripAdvisor!',
+//   },
+// ];
 
 const Home = () => {
-  useScrollTop();
+  // useScrollTop();
 
   const imageTop = (
     <div className='home-main-image-top-container'>
@@ -95,19 +95,27 @@ const Home = () => {
     </Carousel>
   );
 
+  // const valueList = (
+  //   <div className='home-value-list'>
+  //     {values.map(value => (
+  //       <div className='home-value-container' key={value.title}>
+  //         <h4 className='home-value-title'>{value.title}</h4>
+  //         <p className='home-value-description'>{value.description}</p>
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 
-
-
-  const valueList = (
-    <div className='home-value-list'>
-      {values.map(value => (
-        <div className='home-value-container' key={value.title}>
-          <h4 className='home-value-title'>{value.title}</h4>
-          <p className='home-value-description'>{value.description}</p>
-        </div>
-      ))}
+  const aboutTour = (
+    <div className='about-tour-section'>
+      <div className='about-tour-container'>
+        <h3 className='about-tour-title'>Marvel at the skies over Manhattan,</h3>
+        <p className='about-tour-description'>the unparalleled capital of the world, now seen from the air, adding a touch of magic to your experience!</p>
+        <p className='about-tour-description'>Behold the iconic skyline, featuring Central Park, Rockefeller Center, Time Square, and Grand Central Station – each narrating a unique tale of the city's rich history. Witness the majestic Empire State Building, the timeless Brooklyn Bridge, and the symbolic Wall Street. Take in the modern allure of the newly renovated World Trade Center and the historic Verrazano Bridge connecting Manhattan and Staten Island. Don't miss the Statue of Liberty, a French gift celebrating American Independence, once a gateway to dreams on Ellis Island.</p>
+        <p className='about-tour-description'>This three-dimensional sightseeing adventure offers a fresh perspective on the captivating wonders of Manhattan!</p>
+      </div>
     </div>
-  );
+  )
 
   const requestInfo = (
     <div className='home-request-info-form'>
@@ -144,10 +152,11 @@ const Home = () => {
   return (
     <div className='page-container'>
       {imageTop}
-      <section className='home-why-choose-us-container'>
+      {/* <section className='home-why-choose-us-container'>
         <h3 className='home-why-choose-us-text'>Why Choose Jet Air Blue Planes?</h3>
         {valueList}
-      </section>
+      </section> */}
+      {aboutTour}
       {imagesCarousel(tours)}
       <TourCards title={'Choose a Plane Tour'} tours={tours} />
       <section className='home-request-info-container'>
