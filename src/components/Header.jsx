d78545ffa447043433e5d007a4d911a0ce0d5066
Soohyun Lim <d2axasap@gmail.com>
2024-02-09 12:40:46 -0500

    Merge nav bar into header component. Add next ui dependency.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import NavBar from './NavBar';
+import { menuItems } from './MenuItems';
+import MenuItems from './MenuItems.jsx';
 import { useContext, useEffect, useState } from 'react';
 import { Context } from './contextWrapper';
 import { FormattedMessage } from 'react-intl';
@@ -41,7 +42,7 @@ const Header = () => {
           onClick={() => !isMenuOpen && setIsLanguageOpen(!isLanguageOpen)}
         >
           <img
-            className="inline-block me-4"
+            className="inline-block"
             src={Language}
             alt=""
             width="20"
@@ -75,6 +76,74 @@ const Header = () => {
     );
   };
 
+  const subMenus = menuItems.map((menu) => {
+    return menu.submenu;
+  });
+
+  const NavBarMobile = (
+    <ul>
+      <li className="bg-[#060d17] text-[#b5b7ba] font-semibold -mx-52 px-5 py-2.5 -mt-1 cursor-text">
+        <FormattedMessage
+          id={`header.navbar.menu.${menuItems[1].title}`}
+          defaultMessage="Plane Tours"
+        />
+      </li>
+      {subMenus[1].map((subMenu) => {
+        return (
+          <Link to={subMenu.url}>
+            <li
+              className="bg-[#060d17] -mx-52 px-5 py-2.5 -mt-1 hover:underline"
+              key={subMenu.title}
+            >
+              <FormattedMessage
+                id={`header.navbar.menu.${subMenu.title}`}
+                defaultMessage={subMenu.title}
+              />
+            </li>
+          </Link>
+        );
+      })}
+      <li className="bg-[#060d17] text-[#b5b7ba] font-semibold -mx-52 px-5 py-2.5 -mt-1 cursor-text">
+        <FormattedMessage
+          id={`header.navbar.menu.${menuItems[2].title}`}
+          defaultMessage="More"
+        />
+      </li>
+      {subMenus[2].map((subMenu) => {
+        return (
+          <Link to={subMenu.url}>
+            <li
+              className="bg-[#060d17] -mx-52 px-5 py-2.5 -mt-1 hover:underline last:rounded-b-md"
+              key={subMenu.title}
+            >
+              <FormattedMessage
+                id={`header.navbar.menu.${subMenu.title}`}
+                defaultMessage={subMenu.title}
+              />
+            </li>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+
+  const NavBarDesktop = (
+    <ul className="navbar-menu-container gap-10">
+      {menuItems.map((menu, index) => {
+        return <MenuItems items={menu} key={index} />;
+      })}
+      <button className="header-button">
+        <FormattedMessage
+          id="button.book-a-plane-tour"
+          defaultMessage="Book a Plane Tour"
+        />
+      </button>
+      <div className="text-[13px] font-bold flex items-center">
+        <FlagDropdown />
+      </div>
+    </ul>
+  );
+
   return (
     <header>
       <div className="header-container">
@@ -82,23 +151,11 @@ const Header = () => {
           {/* <img src='/images/logo.png' alt='logo' /> */}
           jet air blue
         </Link>
-        <div className="my-4 flex gap-10">
-          {!isMobile && (
-            <>
-              <NavBar />
-              <button className="header-button">
-                <FormattedMessage
-                  id="button.book-a-plane-tour"
-                  defaultMessage="Book a Plane Tour"
-                />
-              </button>
-              <div className="text-[13px] font-bold flex items-center">
-                <FlagDropdown />
-              </div>
-            </>
-          )}
+        <div className="flex gap-10">
+          {!isMobile && <div>{NavBarDesktop}</div>}
+
           {isMobile && (
-            <div className="flex items-center">
+            <div className="flex items-center gap-4">
               <div className="inline text-[13px] font-bold ">
                 <FlagDropdown />
               </div>
@@ -114,9 +171,7 @@ const Header = () => {
                   alt="Menu"
                 />
                 {isMenuOpen && (
-                  <div className="absolute z-10 mt-5">
-                    <NavBar dropdown="true" />
-                  </div>
+                  <div className="absolute z-10 mt-5">{NavBarMobile}</div>
                 )}
               </div>
             </div>
